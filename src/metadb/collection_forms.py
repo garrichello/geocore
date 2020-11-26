44d3e75b0f709fe0da708b5db7f81619314496f2
Igor Okladnikov <oig@ngs.ru>
2020-11-26 22:40:13 +0700

    Add organization name sorting and data_url

--- src/metadb/collection_forms.py
+++ src/metadb/collection_forms.py
@@ -1,5 +1,6 @@
 from django.utils.translation import get_language
 from django.utils.translation import gettext_lazy as _
+from django.shortcuts import reverse
 
 from django.forms import (ModelChoiceField, ModelForm, Textarea)
 
@@ -22,10 +23,11 @@ class CollectionForm(ModelForm):
         orgi18n_pk = kwargs.pop('orgi18n_pk', None)
         super(CollectionForm, self).__init__(*args, **kwargs)
 
-        qset = OrganizationI18N.objects.filter(language__code=get_language())
+        qset = OrganizationI18N.objects.filter(language__code=get_language()).order_by('name')
         self.fields['organizationi18n'] = ModelChoiceField(queryset=qset, initial=orgi18n_pk)
         self.fields['organizationi18n'].label = _('Organization')
         self.fields['organizationi18n'].empty_label = self.empty_label
+        self.fields['organizationi18n'].data_url = reverse('metadb:organization_create')
 
 
 class CollectionI18NForm(ModelForm):
