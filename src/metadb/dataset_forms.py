fc5d27594b6bd637c9b531e248795b9c297d2d79
Igor Okladnikov <oig@ngs.ru>
2020-11-27 12:47:10 +0700

    Add data_urls attributes

--- src/metadb/dataset_forms.py
+++ src/metadb/dataset_forms.py
@@ -1,4 +1,5 @@
 from django.forms import (ModelForm, Textarea)
+from django.shortcuts import reverse
 
 from .models import Dataset
 
@@ -20,3 +21,9 @@ class DatasetForm(ModelForm):
 
         for field in self.fields.keys():
             self.fields[field].empty_label = self.empty_label
+
+        self.fields['collection'].data_url = reverse('metadb:collection_create')
+#        self.fields['resolution'].data_url = reverse('metadb:resolution_create')
+#        self.fields['scenario'].data_url = reverse('metadb:scenario_create')
+#        self.fields['data_kind'].data_url = reverse('metadb:datakind_create')
+#        self.fields['file_type'].data_url = reverse('metadb:filetype_create')
