e6227329e6cba7c433b64049d50cc8c75dc344e0
Igor Okladnikov <oig@ngs.ru>
2020-11-29 00:41:01 +0700

    Rearrangement

--- src/metadb/urls.py
+++ src/metadb/urls.py
@@ -5,17 +5,22 @@ from .views import MainView
 from .apiviews import (CollectionApiListView, DatasetApiListView, DataApiListView, 
                        CollectionApiView)
 from .collection_views import (CollectionCreateView, CollectionDeleteView,
-                               CollectionUpdateView, load_organizations)
+                               CollectionUpdateView)
 from .dataset_views import (DatasetCreateView, DatasetDeleteView,
-                            DatasetUpdateView, load_collections, load_resolutions,
-                            load_scenarios, load_datakinds, load_filetypes)
-from .data_views import (DataCreateView, DataDeleteView, DataUpdateView,
-                         load_dataset_resolutions, load_dataset_scenarios,
-                         load_parameter_lvsgroups, load_parameter_lvsnames,
-                         load_parameter_timesteps)
+                            DatasetUpdateView)
+from .data_views import (DataCreateView, DataDeleteView, DataUpdateView)
 
 from .organization_views import (OrganizationCreateView, OrganizationUpdateView,
                                  OrganizationDeleteView)
+
+from .resolution_views import (ResolutionCreateView, ResolutionUpdateView,
+                               ResolutionDeleteView)
+
+from .form_loads import (load_organizations, load_collections, load_resolutions,
+                         load_scenarios, load_datakinds, load_filetypes, 
+                         load_dataset_resolutions, load_dataset_scenarios,
+                         load_parameter_lvsgroups, load_parameter_lvsnames,
+                         load_parameter_timesteps)
 
 app_name = 'metadb'
 urlpatterns = [
@@ -42,14 +47,18 @@ urlpatterns = [
     path('data/<int:pk>/delete/', DataDeleteView.as_view(), name='data_delete'),
     path('data/api/', DataApiListView.as_view(), name='data_api'),
 
-    path('data/form/load-resolutions/', load_dataset_resolutions, name='form_load_resolutions'),
-    path('data/form/load-scenarios/', load_dataset_scenarios, name='form_load_scenarios'),
-    path('data/form/load-timesteps/', load_parameter_timesteps, name='form_load_timesteps'),
-    path('data/form/load-lvsgroups/', load_parameter_lvsgroups, name='form_load_lvsgroups'),
-    path('data/form/load-lvsnames/', load_parameter_lvsnames, name='form_load_lvsnames'),
+    path('data/form/load-resolutions/', load_dataset_resolutions, name='form_load_dataset_resolutions'),
+    path('data/form/load-scenarios/', load_dataset_scenarios, name='form_load_dataset_scenarios'),
+    path('data/form/load-timesteps/', load_parameter_timesteps, name='form_load_parameter_timesteps'),
+    path('data/form/load-lvsgroups/', load_parameter_lvsgroups, name='form_load_parameter_lvsgroups'),
+    path('data/form/load-lvsnames/', load_parameter_lvsnames, name='form_load_parameter_lvsnames'),
 
     path('organizations/create/', OrganizationCreateView.as_view(), name='organization_create'),
     path('organizations/<int:pk>/update/', OrganizationUpdateView.as_view(), name='organization_update'),
     path('organizations/<int:pk>/delete/', OrganizationDeleteView.as_view(), name='organization_delete'),
+
+    path('resolutions/create/', ResolutionCreateView.as_view(), name='resolution_create'),
+    path('resolutions/<int:pk>/update/', ResolutionUpdateView.as_view(), name='resolution_update'),
+    path('resolutions/<int:pk>/delete/', ResolutionDeleteView.as_view(), name='resolution_delete'),
 
 ]
