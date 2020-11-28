9aa63406efd017a567806773d21dc41de20eeb73
Igor Okladnikov <oig@ngs.ru>
2020-11-29 00:40:39 +0700

    Enabled resolution url

--- src/metadb/dataset_forms.py
+++ src/metadb/dataset_forms.py
@@ -1,7 +1,7 @@
 from django.forms import (ModelForm, Textarea)
 from django.shortcuts import reverse
 
-from .models import Dataset
+from .models import Dataset, Collection, Resolution, Scenario, DataKind, FileType
 
 
 class DatasetForm(ModelForm):
@@ -22,8 +22,13 @@ class DatasetForm(ModelForm):
         for field in self.fields.keys():
             self.fields[field].empty_label = self.empty_label
 
+        self.fields['collection'].queryset = Collection.objects.order_by('label')
         self.fields['collection'].data_url = reverse('metadb:collection_create')
-#        self.fields['resolution'].data_url = reverse('metadb:resolution_create')
+        self.fields['resolution'].queryset = Resolution.objects.order_by('name')
+        self.fields['resolution'].data_url = reverse('metadb:resolution_create')
+        self.fields['scenario'].queryset = Scenario.objects.order_by('name')
 #        self.fields['scenario'].data_url = reverse('metadb:scenario_create')
+        self.fields['data_kind'].queryset = DataKind.objects.order_by('name')
 #        self.fields['data_kind'].data_url = reverse('metadb:datakind_create')
+        self.fields['file_type'].queryset = FileType.objects.order_by('name')
 #        self.fields['file_type'].data_url = reverse('metadb:filetype_create')
