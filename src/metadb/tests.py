793d53981bcdefde93547ad5c7f3a42001290c75
Igor Okladnikov <oig@ngs.ru>
2020-11-29 23:21:50 +0700

    Test home page

--- src/metadb/tests.py
+++ src/metadb/tests.py
@@ -3,6 +3,7 @@ import os
 import django
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 from metadb.views import MainView
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geocore.settings.development')
@@ -13,3 +14,10 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/en-us/metadb/')
         self.assertEqual(found.func.view_class, MainView)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        request.method = 'GET'
+        response = MainView.as_view()(request)
+        html = response.content.decode('utf8')
+        self.assertIn('<title>MetaDB administrative console</title>', html)
