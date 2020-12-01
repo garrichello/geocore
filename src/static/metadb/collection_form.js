80f1124b88566b97954e5f4fdb72629a2b6f44af
Igor Okladnikov <oig@ngs.ru>
2020-12-01 21:49:08 +0700

    Organization name length check before AJAX

--- src/static/metadb/collection_form.js
+++ src/static/metadb/collection_form.js
@@ -5,18 +5,20 @@ $(function() {
         var form = $(form_name);
         var modal_id = '#'+getModalName(form.attr('action')); // Collection modal!
 
-        $.ajax( {
-            url: form.attr('organizations-url'),
-            type: 'get',
-            success: function (data) {
-                $(modal_id+' #id_organizationi18n').html(data);  // Get organizations names
-                $(modal_id+` #id_organizationi18n option:contains("${organization_name}")`).attr(
-                    'selected', true);  // Select new organization in the select
-                if ($(modal_id+' #id_organizationi18n option').length == 2) {
-                    $(modal_id+' #id_organizationi18n').prop("selectedIndex", 1);
+        if (organization_name.length) {
+            $.ajax( {
+                url: form.attr('organizations-url'),
+                type: 'get',
+                success: function (data) {
+                    $(modal_id+' #id_organizationi18n').html(data);  // Get organizations names
+                    $(modal_id+` #id_organizationi18n option:contains("${organization_name}")`).attr(
+                        'selected', true);  // Select new organization in the select
+                    if ($(modal_id+' #id_organizationi18n option').length == 2) {
+                        $(modal_id+' #id_organizationi18n').prop("selectedIndex", 1);
+                    }
                 }
-            }
-        } );
+            } );
+        }
     };
 
     // Submit form
@@ -29,10 +31,12 @@ $(function() {
         var modal_id = loadForm2.call(this);
         $(modal_id).on('hidden.bs.modal', function() {
             var form_data = mapFormData('.js-organization-create-form');  // Get fields of the form as a map
-            if (form_data['name'].length) {
-                loadOrganizations.call(this, collection_form_class_name, form_data['name']);
+            if ('name' in form_data) {
+                if (form_data['name'].length) {
+                    loadOrganizations.call(this, collection_form_class_name, form_data['name']);
+                }
+                $(modal_id).remove();
             }
-            $(modal_id).remove();
         });
     });
 });
