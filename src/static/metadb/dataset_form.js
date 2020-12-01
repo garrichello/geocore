80957e34159da4681ef14814567ac8298bf2078b
Igor Okladnikov <oig@ngs.ru>
2020-12-01 21:49:52 +0700

    Don't AJAX if nothing added

--- src/static/metadb/dataset_form.js
+++ src/static/metadb/dataset_form.js
@@ -6,94 +6,100 @@ $(function() {
         var modal_id = '#'+getModalName(form.attr('action')); // Dataset modal!
         console.log('[loadCollections] modal_id: '+modal_id);
 
-        $.ajax( {
-            url: form.attr('collections-url'),
-            type: 'get',
-            success: function (data) {
-                $(modal_id+' #id_collection').html(data);
-                if (collection_label.length) {
+        if (collection_label.length) {
+            $.ajax( {
+                url: form.attr('collections-url'),
+                type: 'get',
+                success: function(data) {
+                    $(modal_id+' #id_collection').html(data);
                     $(modal_id+` #id_collection option:contains("${collection_label}")`).attr(
                         'selected', true);  // Select new collection in the select
-                }
-                if ($(modal_id+' #id_collection option').length == 2) {
-                    $(modal_id+' #id_collection').prop("selectedIndex", 1);
-                }
-            }
-        } );
+                    if ($(modal_id+' #id_collection option').length == 2) {
+                        $(modal_id+' #id_collection').prop("selectedIndex", 1);
+                    }
+                },
+            } );
+        }
     };
 
     var loadResolutions = function(form_name, resolution_name='') {
         var form = $(form_name);
         var modal_id = '#'+getModalName(form.attr('action')); // Dataset modal!
 
-        $.ajax( {
-            url: form.attr('resolutions-url'),
-            type: 'get',
-            success: function (data) {
-                $(modal_id+' #id_resolution').html(data);
-                if (resolution_name.length) {
-                    $(modal_id+` #id_resolution option:contains("${resolution_name}")`).attr(
-                        'selected', true);  // Select new collection in the select
+        if (resolution_name.length) {
+            $.ajax( {
+                url: form.attr('resolutions-url'),
+                type: 'get',
+                success: function(data) {
+                    $(modal_id+' #id_resolution').html(data);
+                        $(modal_id+` #id_resolution option:contains("${resolution_name}")`).attr(
+                            'selected', true);  // Select new collection in the select
+                    if ($(modal_id+' #id_resolution option').length == 2) {
+                        $(modal_id+' #id_resolution').prop("selectedIndex", 1);
+                    }
                 }
-                if ($(modal_id+' #id_resolution option').length == 2) {
-                    $(modal_id+' #id_resolution').prop("selectedIndex", 1);
-                }
-            }
-        } );
+            } );
+        }
     };
 
     var loadScenarios = function(form_name, scenario_name='') {
         var form = $(form_name);
         var modal_id = '#'+getModalName(form.attr('action')); // Dataset modal!
 
-        $.ajax( {
-            url: form.attr('scenarios-url'),
-            type: 'get',
-            success: function (data) {
-                $(modal_id+' #id_scenario').html(data);
-                $(modal_id+` #id_scenario option:contains("${scenario_name}")`).attr(
-                    'selected', true);  // Select new collection in the select
-                if ($(modal_id+' #id_scenario option').length == 2) {
-                    $(modal_id+' #id_scenario').prop("selectedIndex", 1);
+        if (scenario_name.length) {
+            $.ajax( {
+                url: form.attr('scenarios-url'),
+                type: 'get',
+                success: function (data) {
+                    $(modal_id+' #id_scenario').html(data);
+                    $(modal_id+` #id_scenario option:contains("${scenario_name}")`).attr(
+                        'selected', true);  // Select new collection in the select
+                    if ($(modal_id+' #id_scenario option').length == 2) {
+                        $(modal_id+' #id_scenario').prop("selectedIndex", 1);
+                    }
                 }
-            }
-        } );
+            } );
+        }
     };
 
     var loadDatakinds = function(form_name, datakind_name='') {
         var form = $(form_name);
         var modal_id = '#'+getModalName(form.attr('action')); // Dataset modal!
 
-        $.ajax( {
-            url: form.attr('datakinds-url'),
-            type: 'get',
-            success: function (data) {
-                $(modal_id+' #id_data_kind').html(data);
-                $(modal_id+` #id_data_kind option:contains("${datakind_name}")`).attr(
-                    'selected', true);  // Select new collection in the select
-                if ($(modal_id+' #id_data_kind option').length == 2) {
-                    $(modal_id+' #id_data_kind').prop("selectedIndex", 1);
+        if (datakind_name.length) {
+            $.ajax( {
+                url: form.attr('datakinds-url'),
+                type: 'get',
+                success: function (data) {
+                    $(modal_id+' #id_data_kind').html(data);
+                    $(modal_id+` #id_data_kind option:contains("${datakind_name}")`).attr(
+                        'selected', true);  // Select new collection in the select
+                    if ($(modal_id+' #id_data_kind option').length == 2) {
+                        $(modal_id+' #id_data_kind').prop("selectedIndex", 1);
+                    }
                 }
-            }
-        } );
+            } );
+        }
     };
 
     var loadFiletypes = function(form_name, filetype_name='') {
         var form = $(form_name);
         var modal_id = '#'+getModalName(form.attr('action')); // Dataset modal!
 
-        $.ajax( {
-            url: form.attr('filetypes-url'),
-            type: 'get',
-            success: function (data) {
-                $(modal_id+' #id_file_type').html(data);
-                $(modal_id+` #id_file_type option:contains("${filetype_name}")`).attr(
-                    'selected', true);  // Select new collection in the select
-                if ($(modal_id+' #id_file_type option').length == 2) {
-                    $(modal_id+' #id_file_type').prop("selectedIndex", 1);
+        if (filetype_name.length) {
+            $.ajax( {
+                url: form.attr('filetypes-url'),
+                type: 'get',
+                success: function (data) {
+                    $(modal_id+' #id_file_type').html(data);
+                    $(modal_id+` #id_file_type option:contains("${filetype_name}")`).attr(
+                        'selected', true);  // Select new collection in the select
+                    if ($(modal_id+' #id_file_type option').length == 2) {
+                        $(modal_id+' #id_file_type').prop("selectedIndex", 1);
+                    }
                 }
-            }
-        } );
+            } );
+        }
     };
 
     // Submit form
@@ -110,32 +116,22 @@ $(function() {
             if ($('.js-collection-create-form').length) {
                 var form_data = mapFormData('.js-collection-create-form');  // Get collection fields
                 loadCollections.call(this, dataset_form_class_name, form_data['label']);
-                console.log('In dataset modal, form data: '+form_data['label']);
+                console.log('In dataset modal, collection form data: '+form_data['label']);
             };
             if ($('.js-resolution-create-form').length) {
-                var form_data = mapFormData('.js-resolution-create-form');  // Get collection fields
+                var form_data = mapFormData('.js-resolution-create-form');  // Get resolution fields
                 loadResolutions.call(this, dataset_form_class_name, form_data['name']);
-                console.log('In dataset modal, form data: '+form_data['name']);
+                console.log('In dataset modal, resolution form data: '+form_data['name']);
+            };
+            if ($('.js-scenario-create-form').length) {
+                var form_data = mapFormData('.js-scenario-create-form');  // Get scenario fields
+                loadScenarios.call(this, dataset_form_class_name, form_data['name']);
+                console.log('In dataset modal, scenario form data: '+form_data['name']);
             };
             $(modal_id).remove();
         })
     });
 
-    var resolution_form_class = '.js-resolution-create-form';
-    $('body').on('submit', resolution_form_class, function(e) {
-        saveForm2.call(this, e);
-        var form_data = mapFormData(resolution_form_class);  // Get fields of the form as a map
-        loadResolutions.call(this, dataset_form_class_name, form_data['name']);
-        return false; 
-    });
-
-    var scenario_form_class = '.js-scenario-create-form';
-    $('body').on('submit', scenario_form_class, function(e) {
-        saveForm2.call(this, e);
-        var form_data = mapFormData(scenario_form_class);  // Get fields of the form as a map
-        loadScenarios.call(this, dataset_form_class_name, form_data['name']);
-        return false; 
-    });
 
     var datakind_form_class = '.js-datakind-create-form';
     $('body').on('submit', datakind_form_class, function(e) {
