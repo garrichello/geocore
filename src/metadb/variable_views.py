3251bcf3f4df2137fa32a294b2a01f4c5d55657b
Igor Okladnikov <oig@ngs.ru>
2020-12-04 01:48:00 +0700

    New Variable views

---
+++ src/metadb/variable_views.py
@@ -0,0 +1,42 @@
+from .simple_views import SimpleCreateView, SimpleUpdateView, SimpleDeleteView
+from django.utils.translation import gettext_lazy as _
+
+from .simple_forms import VariableForm
+
+from .models import Variable
+
+
+class VariableCreateView(SimpleCreateView):
+    form_class = VariableForm
+    model = Variable
+    template_name = 'metadb/includes/simple_form.html'
+    ctx = {
+        'form_class': 'js-variable-create-form',
+        'title': _("Create a new variable"),
+        'submit_name': _("Create variable"),
+    }
+    url_name = 'metadb:variable_create'
+
+
+class VariableUpdateView(SimpleUpdateView):
+    form_class = VariableForm
+    model = Variable
+    template_name = 'metadb/includes/simple_form.html'
+    ctx = {
+        'form_class': 'js-variable-update-form',
+        'title': _("Update variable"),
+        'submit_name': _("Update variable"),
+    }
+    url_name = 'metadb:variable_update'
+
+class VariableDeleteView(SimpleDeleteView):
+    form_class = VariableForm
+    model = Variable
+    template_name = 'metadb/includes/simple_delete_form.html'
+    ctx = {
+        'form_class': 'js-variable-delete-form',
+        'title': _('Confirm variable delete'),
+        'text': _('Are you sure you want to delete the variable'),
+        'submit_name': _('Delete variable')
+    }
+    url_name = 'metadb:variable_delete'
