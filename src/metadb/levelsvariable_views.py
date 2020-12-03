73ca8f5a1aa5ae035c55f4a5db458a29c99310d3
Igor Okladnikov <oig@ngs.ru>
2020-12-04 01:45:30 +0700

    New levels variable views

---
+++ src/metadb/levelsvariable_views.py
@@ -0,0 +1,42 @@
+from .simple_views import SimpleCreateView, SimpleUpdateView, SimpleDeleteView
+from django.utils.translation import gettext_lazy as _
+
+from .simple_forms import LevelsVariableForm
+
+from .models import Variable
+
+
+class LevelsVariableCreateView(SimpleCreateView):
+    form_class = LevelsVariableForm
+    model = Variable
+    template_name = 'metadb/includes/simple_form.html'
+    ctx = {
+        'form_class': 'js-levels-variable-create-form',
+        'title': _("Create a new levels variable"),
+        'submit_name': _("Create levels variable"),
+    }
+    url_name = 'metadb:levels_variable_create'
+
+
+class LevelsVariableUpdateView(SimpleUpdateView):
+    form_class = LevelsVariableForm
+    model = Variable
+    template_name = 'metadb/includes/simple_form.html'
+    ctx = {
+        'form_class': 'js-levels-variable-update-form',
+        'title': _("Update levels variable"),
+        'submit_name': _("Update levels variable"),
+    }
+    url_name = 'metadb:levels_variable_update'
+
+class LevelsVariableDeleteView(SimpleDeleteView):
+    form_class = LevelsVariableForm
+    model = Variable
+    template_name = 'metadb/includes/simple_delete_form.html'
+    ctx = {
+        'form_class': 'js-levels-variable-delete-form',
+        'title': _('Confirm levels variable delete'),
+        'text': _('Are you sure you want to delete the levels variable'),
+        'submit_name': _('Delete levels variable')
+    }
+    url_name = 'metadb:levels_variable_delete'
