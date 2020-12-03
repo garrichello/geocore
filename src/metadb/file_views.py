ee2cd65e940a108c9a4646a53641a2f80476e82f
Igor Okladnikov <oig@ngs.ru>
2020-12-04 01:44:22 +0700

    New file views

---
+++ src/metadb/file_views.py
@@ -0,0 +1,42 @@
+from .simple_views import SimpleCreateView, SimpleUpdateView, SimpleDeleteView
+from django.utils.translation import gettext_lazy as _
+
+from .simple_forms import FileForm
+
+from .models import File
+
+
+class FileCreateView(SimpleCreateView):
+    form_class = FileForm
+    model = File
+    template_name = 'metadb/includes/simple_form.html'
+    ctx = {
+        'form_class': 'js-file-create-form',
+        'title': _("Create a new file"),
+        'submit_name': _("Create file"),
+    }
+    url_name = 'metadb:file_create'
+
+
+class FileUpdateView(SimpleUpdateView):
+    form_class = FileForm
+    model = File
+    template_name = 'metadb/includes/simple_form.html'
+    ctx = {
+        'form_class': 'js-file-update-form',
+        'title': _("Update file"),
+        'submit_name': _("Update file"),
+    }
+    url_name = 'metadb:file_update'
+
+class FileDeleteView(SimpleDeleteView):
+    form_class = FileForm
+    model = File
+    template_name = 'metadb/includes/simple_delete_form.html'
+    ctx = {
+        'form_class': 'js-file-delete-form',
+        'title': _('Confirm file delete'),
+        'text': _('Are you sure you want to delete the file'),
+        'submit_name': _('Delete file')
+    }
+    url_name = 'metadb:file_delete'
