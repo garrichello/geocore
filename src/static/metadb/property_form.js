79e65433b4d44a9ebc9af1036b545c525e65b377
Igor Okladnikov <oig@ngs.ru>
2020-12-04 23:56:53 +0700

    New property form JS

---
+++ src/static/metadb/property_form.js
@@ -0,0 +1,39 @@
+$(function() {
+    var loadOptions = function(form_name, select_name, data_url, option_name='') {
+        var form = $(form_name);
+        var modal_id = '#'+getModalName(form.attr('action'));
+
+        if (option_name.length) {
+            $.ajax( {
+                url: form.attr(data_url),
+                type: 'get',
+                success: function (data) {
+                    $(`${modal_id} #${select_name}`).html(data);
+                    $(`${modal_id} #${select_name} option`).filter(function() {
+                        return $(this).text() === option_name;
+                    }).attr('selected', true);  // Select given entry in the select
+                    if ($(`${modal_id} #${select_name} option`).length == 2) {
+                        $(`${modal_id} #${select_name}`).prop("selectedIndex", 1);
+                    }
+                }
+            } );
+        }
+    };
+
+    // Create organization modal
+    var simple_form_class_name = '.js-property-create-form';
+    $(simple_form_class_name).on('click', '.js-add-button', function() { 
+        var modal_id = loadForm2.call(this);
+        $(modal_id).on('hidden.bs.modal', function() {
+            var form_data = mapFormData('.js-gui-element-create-form');  // Get fields of the form as a map
+            if ('name' in form_data) {
+                if (form_data['name'].length) {
+                    loadOptions.call(this, simple_form_class_name, 'id_gui_element', 
+                        'gui-element-url', form_data['name']
+                    );
+                }
+                $(modal_id).remove();
+            }
+        });
+    });
+});
