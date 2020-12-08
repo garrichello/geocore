b302f4a01d77904fe7ddcbf71d8c2e1a8a88b7db
Igor Okladnikov <oig@ngs.ru>
2020-12-09 00:18:08 +0700

    Add Levels list creation support

--- src/static/metadb/levelsgroup_form.js
+++ src/static/metadb/levelsgroup_form.js
@@ -1,17 +1,97 @@
 $(function() {
     var levels_group_form_class_name = '.js-levels-group-form';
+    var modal_id = '#'+getModalName();
+
+    var loadLvsNames = function() {
+        var form = $(levels_group_form_class_name);
+    
+        $.ajax( { 
+            url: form.attr('levels-url'),  // load lvsnames
+            type: 'get',
+            success: function (data) {
+                $(modal_id+' #available_levels_list').html(data);
+            }
+        } );
+    };
+
+    $(levels_group_form_class_name).ready(loadLvsNames());
+
+    var sortLevels = function(listId) {
+        var mylist = $(listId);
+        var listitems = mylist.children('a').get();
+        listitems.sort(function(a, b) {
+           return $(a).text().toUpperCase().localeCompare($(b).text().toUpperCase());
+        })
+        $.each(listitems, function(idx, itm) { mylist.append(itm); });        
+    }
+
+    var getLi = function(action, name) {
+        return $(`<a href="#" class="list-group-item js-${action}-level-button">${name}</a>`);
+    }
+
+    // Store current list of selected levels in the hidden input
+    var storeList = function() {
+        var list = $.map($(modal_id+' #selected_levels_list a'), function(n, i) {
+            return ($(n).text());
+        });
+        $(modal_id+' #id_selected_levels').val(JSON.stringify(list));
+    }
+
+    var addLevel = function(obj) {
+        var name = $(obj).text();
+        $(modal_id+' #selected_levels_list').append(getLi('remove', name));
+        $(obj).remove();
+        storeList();
+    }
+
+    var removeLevel = function(obj) {
+        var name = $(obj).text();
+        $(modal_id+' #available_levels_list').append($(getLi('add', name)));
+        $(obj).remove();
+        storeList();
+    }
+
+    // Add one level
+    $(levels_group_form_class_name).on('click', '.js-add-level-button', function() {
+        addLevel(this);
+        sortLevels('#selected_levels_list');
+    });
+
+    // Add all levels
+    $(levels_group_form_class_name).on('click', '#js-add-all-levels-button', function() {
+        $.each($(modal_id+' #available_levels_list a'), function(index, value) {
+            addLevel(value);
+        });
+        sortLevels('#selected_levels_list');
+    });
+
+    // Remove one level
+    $(levels_group_form_class_name).on('click', '.js-remove-level-button', function() {
+        removeLevel(this);
+        sortLevels('#available_levels_list');
+    });
+
+    // Remove all levels
+    $(levels_group_form_class_name).on('click', '#js-remove-all-levels-button', function() {
+        $.each($(modal_id+' #selected_levels_list a'), function(index, value) {
+            removeLevel(value);
+        });
+        sortLevels('#available_levels_list');
+    });
 
     // + buttons handling
     $(levels_group_form_class_name).on('click', '.js-add-button', function() {
-        var modal_id = loadForm2.call(this);
-        $(modal_id).on('hidden.bs.modal', function() {
-            if ($('.js-unit-form').length) {
-                var form_data = mapFormData('.js-unit-form');  // Get units fields
-                loadOptions.call( this, levels_group_form_class_name, 'id_unitsi18n',
-                    'units-url', form_data['name']
-                );
+        var child_modal_id = loadForm2.call(this);
+        var dismissed = false;
+        $(child_modal_id).on('click.dismiss.bs.modal', '[data-dismiss="modal"]', function() {
+            dismissed = true;
+        })
+        $(child_modal_id).on('hidden.bs.modal', function() {
+            if ($('.js-level-form').length && !dismissed) {
+                var form_data = mapFormData('.js-level-form');  // Get level fields
+                $(modal_id+' #available_levels_list').append(getLi(form_data['label']));
             };
-            $(modal_id).remove();
+            $(child_modal_id).remove();
         })
     });
 });
