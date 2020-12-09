6d97f6d9b9f8d4f6dd8c8b8468c8314db1b4f29f
Igor Okladnikov <oig@ngs.ru>
2020-12-09 13:06:42 +0700

    New template filter print_static

---
+++ src/metadb/templatetags/metadb_extras.py
@@ -0,0 +1,12 @@
+from django import template
+from django.contrib.staticfiles import finders
+
+register = template.Library()
+
+@register.filter
+def print_static(name):
+    try:
+        with open(finders.find(name), 'r') as f:
+            return f.read()
+    except IOError:
+        return ''
