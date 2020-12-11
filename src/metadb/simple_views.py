a8aee8c2481d3ea1b1fd859c0a18637cdcc6fd25
Igor Okladnikov <oig@ngs.ru>
2020-12-11 14:13:20 +0700

    Add Levels variable class

--- src/metadb/simple_views.py
+++ src/metadb/simple_views.py
@@ -193,6 +193,42 @@ class LanguageDeleteView(CommonDeleteView):
     url_name = 'metadb:language_delete'
 
 
+class LevelsVariableCreateView(CommonCreateView):
+    form_class = LevelsVariableForm
+    model = Variable
+    template_name = 'metadb/includes/simple_form.html'
+    ctx = {
+        'form_class': 'js-levels-variable-form',
+        'title': _("Create a new levels variable"),
+        'submit_name': _("Create levels variable"),
+    }
+    url_name = 'metadb:levels_variable_create'
+
+
+class LevelsVariableUpdateView(CommonUpdateView):
+    form_class = LevelsVariableForm
+    model = Variable
+    template_name = 'metadb/includes/simple_form.html'
+    ctx = {
+        'form_class': 'js-levels-variable-form',
+        'title': _("Update levels variable"),
+        'submit_name': _("Update levels variable"),
+    }
+    url_name = 'metadb:levels_variable_update'
+
+class LevelsVariableDeleteView(CommonDeleteView):
+    form_class = LevelsVariableForm
+    model = Variable
+    template_name = 'metadb/includes/delete_form.html'
+    ctx = {
+        'form_class': 'js-levels-variable-delete-form',
+        'title': _('Confirm levels variable delete'),
+        'text': _('Are you sure you want to delete the levels variable'),
+        'submit_name': _('Delete levels variable')
+    }
+    url_name = 'metadb:levels_variable_delete'
+    
+    
 class PropertyCreateView(CommonCreateView):
     form_class = PropertyForm
     model = Property
