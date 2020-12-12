97e9133ec9b4ecac9a2153fbc37c4d117b3c9a2e
Igor Okladnikov <oig@ngs.ru>
2020-12-12 21:56:18 +0700

    Refactoring. Inherited from Common2 classes now

--- src/metadb/guielement_views.py
+++ src/metadb/guielement_views.py
@@ -1,107 +1,43 @@
-from django.views import View
-from django.template.loader import render_to_string
-from django.http import JsonResponse
-from django.utils.translation import get_language, gettext_lazy as _
-from django.shortcuts import get_object_or_404
-from django.urls import reverse, reverse_lazy
+from django.utils.translation import gettext_lazy as _
 
 from .simple_forms import GuiElementForm, GuiElementI18NForm
 
-from .models import GuiElement, Language
+from .models import GuiElement
 
-class GuiElementBaseView(View):
+from .common2_views import Common2CreateView, Common2UpdateView, Common2DeleteView
+
+
+class GuiElementCreateView(Common2CreateView):
     form_class = GuiElementForm
     formi18n_class = GuiElementI18NForm
     model = GuiElement
-
-    def save_form(self, request, template_name, ctx, create=False):
-        ''' Saves the form
-        create -- True if creating, False if updating.
-        '''
-        data = dict()
-        gui_form = ctx['forms'][0]
-        guii18n_form = ctx['forms'][1]
-        if gui_form.is_valid() and guii18n_form.is_valid():
-            # Get GUI element object save.
-            gui = gui_form.save()
-            guii18n = guii18n_form.save(commit=False)  # Get i18n GUI element object
-            guii18n.gui_element = gui  # Link it with the new GUI element
-            # To save DB consistency we create a new record for all languages.
-            # User can update/translate to every other language lately and separately.
-            if create:
-                for db_lang in Language.objects.all():  # Iterate over all languages in DB
-                    guii18n.language = db_lang  # Link it with an existing language
-                    guii18n.pk = None  # Clear PK to save data into a new record
-                    guii18n.save()  # Save
-            else:  # Just update the existing record.
-                guii18n.save()
-
-            guii18n_form.save_m2m()  # Save form and many-to-many relations
-            data['form_is_valid'] = True
-        else:
-            data['form_is_valid'] = False
-
-        data['html_form'] = render_to_string(template_name, ctx, request)
-        return JsonResponse(data)
-
-    def get_models(self, pk):
-        ''' Just get additional model objects for a given PK '''
-        model_obj = get_object_or_404(self.model, pk=pk)
-        modeli18n_obj = model_obj.guielementi18n_set.filter(language__code=get_language()).get()
-        return model_obj, modeli18n_obj
-
-
-class GuiElementCreateView(GuiElementBaseView):
     template_name = 'metadb/includes/simple_form.html'
     ctx = {
         'form_class': 'js-gui-element-form',
-        'action': reverse_lazy('metadb:gui_element_create'),
         'title': _("Create a new GUI element"),
         'submit_name': _("Create GUI element"),
     }
-
-    def get(self, request):
-        gui_form = self.form_class()
-        guii18n_form = self.formi18n_class()
-
-        self.ctx['forms'] = [gui_form, guii18n_form]
-        html_form = render_to_string(self.template_name, self.ctx, request)
-        return JsonResponse({'html_form': html_form})
-
-    def post(self, request):
-        gui_form = self.form_class(request.POST)
-        guii18n_form = self.formi18n_class(request.POST)
-        self.ctx['forms'] = [gui_form, guii18n_form]
-        return self.save_form(request, self.template_name, self.ctx, create=True)
+    action_url = 'metadb:gui_element_create'
+    fk_field_name = 'gui_element'
 
 
-class GuiElementUpdateView(GuiElementBaseView):
+class GuiElementUpdateView(Common2UpdateView):
+    form_class = GuiElementForm
+    formi18n_class = GuiElementI18NForm
+    model = GuiElement
     template_name = 'metadb/includes/simple_form.html'
     ctx = {
         'form_class': 'js-gui-element-form',
         'title': _("Update GUI element"),
         'submit_name': _("Update GUI element"),
     }
+    action_url = 'metadb:gui_element_update'
+    fk_field_name = 'gui_element'
+    reverse_field_name = 'guielementi18n'
 
-    def get(self, request, pk):
-        gui_model, guii18n_model = self.get_models(pk)
-        gui_form = self.form_class(instance=gui_model)
-        guii18n_form = self.formi18n_class(instance=guii18n_model)
 
-        self.ctx['forms'] = [gui_form, guii18n_form]
-        self.ctx['action'] = reverse('metadb:gui_element_update', kwargs={'pk': gui_form.instance.pk}),
-        html_form = render_to_string(self.template_name, self.ctx, request)
-        return JsonResponse({'html_form': html_form})
-
-    def post(self, request, pk):
-        gui_model_old, guii18n_model_old = self.get_models(pk)
-        gui_form = self.form_class(request.POST, instance=gui_model_old)
-        guii18n_form = self.formi18n_class(request.POST, instance=guii18n_model_old)
-        self.ctx['forms'] = [gui_form, guii18n_form]
-        self.ctx['action'] = reverse('metadb:gui_element_update', kwargs={'pk': gui_form.instance.pk}),
-        return self.save_form(request, self.template_name, self.ctx)
-
-class GuiElementDeleteView(GuiElementBaseView):
+class GuiElementDeleteView(Common2DeleteView):
+    model = GuiElement
     template_name = 'metadb/includes/delete_form.html'
     ctx = {
         'form_class': 'js-gui-element-delete-form',
@@ -109,15 +45,4 @@ class GuiElementDeleteView(GuiElementBaseView):
         'text': _('Are you sure you want to delete the GUI element'),
         'submit_name': _('Delete GUI element')
     }
-
-    def get(self, request, pk):
-        model_obj = get_object_or_404(self.model, pk=pk)
-        self.ctx['action'] = reverse('metadb:gui_element_delete', kwargs={'pk': pk})
-        self.ctx['label'] = model_obj.label
-        html_form = render_to_string(self.template_name, self.ctx, request)
-        return JsonResponse({'html_form': html_form})
-
-    def post(self, request, pk):
-        gui_model = get_object_or_404(self.model, pk=pk)
-        gui_model.delete()
-        return JsonResponse({'html_form': None, 'form_is_valid': True})      
+    action_url = 'metadb:gui_element_delete'
--- src/metadb/organization_views.py
+++ src/metadb/organization_views.py
@@ -1,107 +1,43 @@
-from django.views import View
-from django.template.loader import render_to_string
-from django.http import JsonResponse
-from django.utils.translation import get_language, gettext_lazy as _
-from django.shortcuts import get_object_or_404
-from django.urls import reverse, reverse_lazy
+from django.utils.translation import gettext_lazy as _
+
+from .common2_views import Common2CreateView, Common2UpdateView, Common2DeleteView
 
 from .simple_forms import OrganizationForm, OrganizationI18NForm
 
-from .models import Organization, Language
+from .models import Organization
 
-class OrganizationBaseView(View):
+
+class OrganizationCreateView(Common2CreateView):
     form_class = OrganizationForm
     formi18n_class = OrganizationI18NForm
     model = Organization
-
-    def save_form(self, request, template_name, ctx, create=False):
-        ''' Saves the form
-        create -- True if creating, False if updating.
-        '''
-        data = dict()
-        orgi18n_form = ctx['forms'][0]
-        org_form = ctx['forms'][1]
-        if org_form.is_valid() and orgi18n_form.is_valid():
-            # Get organization object save.
-            org = org_form.save()
-            orgi18n = orgi18n_form.save(commit=False)  # Get i18n organization object
-            orgi18n.organization = org  # Link it with the new organization
-            # To save DB consistency we create a new record for all languages.
-            # User can update/translate to every other language lately and separately.
-            if create:
-                for db_lang in Language.objects.all():  # Iterate over all languages in DB
-                    orgi18n.language = db_lang  # Link it with an existing language
-                    orgi18n.pk = None  # Clear PK to save data into a new record
-                    orgi18n.save()  # Save
-            else:  # Just update the existing record.
-                orgi18n.save()
-
-            orgi18n_form.save_m2m()  # Save form and many-to-many relations
-            data['form_is_valid'] = True
-        else:
-            data['form_is_valid'] = False
-
-        data['html_form'] = render_to_string(template_name, ctx, request)
-        return JsonResponse(data)
-
-    def get_models(self, pk):
-        ''' Just get additional model objects for a given PK '''
-        model_obj = get_object_or_404(self.model, pk=pk)
-        modeli18n_obj = model_obj.organizationi18n_set.filter(language__code=get_language()).get()
-        return model_obj, modeli18n_obj
-
-
-class OrganizationCreateView(OrganizationBaseView):
     template_name = 'metadb/includes/simple_form.html'
     ctx = {
         'form_class': 'js-organization-form',
-        'action': reverse_lazy('metadb:organization_create'),
         'title': _("Create a new organization"),
         'submit_name': _("Create organization"),
     }
-
-    def get(self, request):
-        org_form = self.form_class()
-        orgi18n_form = self.formi18n_class()
-
-        self.ctx['forms'] = [orgi18n_form, org_form]
-        html_form = render_to_string(self.template_name, self.ctx, request)
-        return JsonResponse({'html_form': html_form})
-
-    def post(self, request):
-        org_form = self.form_class(request.POST)
-        orgi18n_form = self.formi18n_class(request.POST)
-        self.ctx['forms'] = [orgi18n_form, org_form]
-        return self.save_form(request, self.template_name, self.ctx, create=True)
+    action_url = 'metadb:organization_create'
+    fk_field_name = 'organization'
 
 
-class OrganizationUpdateView(OrganizationBaseView):
+class OrganizationUpdateView(Common2UpdateView):
+    form_class = OrganizationForm
+    formi18n_class = OrganizationI18NForm
+    model = Organization
     template_name = 'metadb/includes/simple_form.html'
     ctx = {
         'form_class': 'js-organization-form',
         'title': _("Update organization"),
         'submit_name': _("Update organization"),
     }
+    action_url = 'metadb:organization_update'
+    fk_field_name = 'organization'
+    reverse_field_name = 'organizationi18n'
 
-    def get(self, request, pk):
-        org_model, orgi18n_model = self.get_models(pk)
-        org_form = self.form_class(instance=org_model)
-        orgi18n_form = self.formi18n_class(instance=orgi18n_model)
 
-        self.ctx['forms'] = [orgi18n_form, org_form]
-        self.ctx['action'] = reverse('metadb:organization_update', kwargs={'pk': org_form.instance.pk}),
-        html_form = render_to_string(self.template_name, self.ctx, request)
-        return JsonResponse({'html_form': html_form})
-
-    def post(self, request, pk):
-        org_model_old, orgi18n_model_old = self.get_models(pk)
-        org_form = self.form_class(request.POST, instance=org_model_old)
-        orgi18n_form = self.formi18n_class(request.POST, instance=orgi18n_model_old)
-        self.ctx['forms'] = [orgi18n_form, org_form]
-        self.ctx['action'] = reverse('metadb:organization_update', kwargs={'pk': org_form.instance.pk}),
-        return self.save_form(request, self.template_name, self.ctx)
-
-class OrganizationDeleteView(OrganizationBaseView):
+class OrganizationDeleteView(Common2DeleteView):
+    model = Organization
     template_name = 'metadb/includes/delete_form.html'
     ctx = {
         'form_class': 'js-organization-delete-form',
@@ -109,15 +45,4 @@ class OrganizationDeleteView(OrganizationBaseView):
         'text': _('Are you sure you want to delete the organization'),
         'submit_name': _('Delete organization')
     }
-
-    def get(self, request, pk):
-        model_obj = get_object_or_404(self.model, pk=pk)
-        self.ctx['action'] = reverse('metadb:organization_delete', kwargs={'pk': pk})
-        self.ctx['label'] = model_obj.label
-        html_form = render_to_string(self.template_name, self.ctx, request)
-        return JsonResponse({'html_form': html_form})
-
-    def post(self, request, pk):
-        org_model = get_object_or_404(self.model, pk=pk)
-        org_model.delete()
-        return JsonResponse({'html_form': None, 'form_is_valid': True})      
+    action_url = 'metadb:organization_delete'
