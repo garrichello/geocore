b340743c324e8bc15e8a93e6fc533a66c504edd6
Igor Okladnikov <oig@ngs.ru>
2021-02-03 12:54:49 +0700

    Adopted for REST API

--- src/metadb/views.py
+++ src/metadb/views.py
@@ -12,45 +12,31 @@ class MainView(View):
 
             {'title': _('Data kind'), 'api_url': app_prfx+'datakind-list'},
 
-            {'title': _('File'), 'api_url': app_prfx+'files_api',
-             'create_url': app_prfx+'file_create',
-             'update_url': app_prfx+'file_update', 'delete_url': app_prfx+'file_delete'},
+            {'title': _('File'), 'api_url': app_prfx+'file-list'},
 
             {'title': _('File type'), 'api_url': app_prfx+'filetype-list'},
 
-            {'title': _('GUI element'), 'api_url': app_prfx+'gui_elements_api',
-             'create_url': app_prfx+'gui_element_create',
-             'update_url': app_prfx+'gui_element_update', 'delete_url': app_prfx+'gui_element_delete'},
+            {'title': _('GUI element'), 'api_url': app_prfx+'guielement-list'},
 
-            {'title': _('Language'), 'api_url': app_prfx+'languages_api',
-             'create_url': app_prfx+'language_create',
-             'update_url': app_prfx+'language_update', 'delete_url': app_prfx+'language_delete'},
+            {'title': _('Language'), 'api_url': app_prfx+'language-list'},
 
             {'title': _('Level'), 'api_url': app_prfx+'level-list'},
 
             {'title': _('Levels group'), 'api_url': app_prfx+'levelsgroup-list'},
 
-            {'title': _('Levels variable'), 'api_url': app_prfx+'levels_variables_api',
-             'create_url': app_prfx+'levels_variable_create',
-             'update_url': app_prfx+'levels_variable_update', 'delete_url': app_prfx+'levels_variable_delete'},
+            {'title': _('Levels variable'), 'api_url': app_prfx+'variable-list'},
 
             {'title': _('Organization'), 'api_url': app_prfx+'organization-list'},
 
             {'title': _('Meteorological parameter'), 'api_url': app_prfx+'parameter-list'},
 
-            {'title': _('Property'), 'api_url': app_prfx+'properties_api',
-             'create_url': app_prfx+'property_create',
-             'update_url': app_prfx+'property_update', 'delete_url': app_prfx+'property_delete'},
+            {'title': _('Property'), 'api_url': app_prfx+'property-list'},
 
-            {'title': _('Property value'), 'api_url': app_prfx+'property_values_api',
-             'create_url': app_prfx+'property_value_create',
-             'update_url': app_prfx+'property_value_update', 'delete_url': app_prfx+'property_value_delete'},
+            {'title': _('Property value'), 'api_url': app_prfx+'propertyvalue-list'},
 
             {'title': _('Resolution'), 'api_url': app_prfx+'resolution-list'},
 
-            {'title': _('Root directory'), 'api_url': app_prfx+'root_dirs_api',
-             'create_url': app_prfx+'root_dir_create',
-             'update_url': app_prfx+'root_dir_update', 'delete_url': app_prfx+'root_dir_delete'},
+            {'title': _('Root directory'), 'api_url': app_prfx+'rootdir-list'},
 
             {'title': _('Scenario'), 'api_url': app_prfx+'scenario-list'},
 
@@ -58,9 +44,7 @@ class MainView(View):
 
             {'title': _('Measurement unit'), 'api_url': app_prfx+'units-list'},
 
-            {'title': _('Variable'), 'api_url': app_prfx+'variables_api',
-             'create_url': app_prfx+'variable_create',
-             'update_url': app_prfx+'variable_update', 'delete_url': app_prfx+'variable_delete'},
+            {'title': _('Variable'), 'api_url': app_prfx+'variable-list'},
         ]
 
         ctx = {
