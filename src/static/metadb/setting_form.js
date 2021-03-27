f83a86b4a9b4daa0a304866fecafd51bc22ce3e1
Igor Okladnikov <oig@ngs.ru>
2021-03-27 13:44:17 +0700

    New Setting form JS

---
+++ src/static/metadb/setting_form.js
@@ -0,0 +1,17 @@
+$(function() {
+    var setting_form_class_name = '.js-setting-form';
+
+    // + buttons handling
+    $(setting_form_class_name).on('click', '.js-add-button', function() {
+        var child_modal_id = loadForm2.call(this, 'create');
+        $(child_modal_id).on('hidden.bs.modal', function() {
+            if ($('.js-gui-element-form').length) {
+                var form_data = mapFormData('.js-gui-element-form');  // Get gui element fields
+                loadOptions.call( this, setting_form_class_name, 'id_gui_element',
+                    'guielements-url', form_data['name']
+                );
+            };
+            $(child_modal_id).remove();
+        })
+    });
+});
