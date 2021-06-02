217fabf43d15e8a8e09000be5232d282e1e5b6ea
Igor <oig@ngs.ru>
2021-06-02 21:54:28 +0700

    Fix

--- src/geocore/settings/base.py
+++ src/geocore/settings/base.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's5$l3hixk*=&ls@vsvpb=3-a$&0=&ggv%w=jz_0%692(zltyba'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
